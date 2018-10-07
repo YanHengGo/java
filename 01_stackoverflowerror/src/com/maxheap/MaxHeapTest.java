31d3d95417dfaab94adf74103ba2af3fd98c9ff9
YanHengGo <dang712@hotmail.com>
2018-10-08 04:06:33 +0900

    checkstyle追加

--- 01_stackoverflowerror/src/com/maxheap/MaxHeapTest.java
+++ 01_stackoverflowerror/src/com/maxheap/MaxHeapTest.java
@@ -4,53 +4,51 @@ import java.util.Vector;
 
 public class MaxHeapTest {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("最大メモリ："+Runtime.getRuntime().maxMemory()/1024/1024+"M");
+  public static void main(String[] args) {
+    System.out.println("最大メモリ：" + Runtime.getRuntime().maxMemory() / 1024 / 1024 + "M");
 
-//		test1();
-//		test2();
-		test3();
+    //test1();
+    //test2();
+    test3();
+  }
 
-	}
+  /*
+   * -Xmn10M -XX:SurvivorRatio=8 -XX:+PrintGCDetails
+   */
+  private static void test3() {
+    // TODO 自動生成されたメソッド・スタブ
 
-	/*
-	 * -Xmn10M -XX:SurvivorRatio=8 -XX:+PrintGCDetails
-	 */
-	private static void test3() {
-		// TODO 自動生成されたメソッド・スタブ
+  }
 
-	}
+  /*
+   * -Xmx5M -Xms1M -XX:+PrintGCDetails
+   */
+  private static void test2() {
+    // TODO 自動生成されたメソッド・スタブ
+    Vector<Byte[]> v = new Vector<Byte[]>();
+    for (int i = 0;i < 10;i++) {
+      Byte[] b = new Byte[1024 * 1024];
+      v.add(b);
+      System.out.println("--------------");
+      if (v.size() == 3) {
+        v.clear();
+        System.out.println("------clear--------");
+      }
+    }
 
-	/*
-	 * -Xmx5M -Xms1M -XX:+PrintGCDetails
-	 */
-	private static void test2() {
-		// TODO 自動生成されたメソッド・スタブ
-		Vector v = new Vector();
-		for(int i=0;i<10;i++) {
-			byte[] b = new byte[1024*1024];
-			v.add(b);
-			System.out.println("--------------");
-			if(v.size() == 3) {
-				v.clear();
-				System.out.println("------clear--------");
-			}
-		}
+  }
 
-	}
-
-	/*
-	 * -Xss2M -Xmx5M
-	 */
-	private static void test1() {
-		// TODO 自動生成されたメソッド・スタブ
-		Vector v = new Vector();
-		for(int i=0;i<10;i++) {
-			byte[] b = new byte[1024*1024];
-			v.add(b);
-			System.out.println((i+1)+"M メモリ利用された");
-		}
-	}
+  /*
+   * -Xss2M -Xmx5M
+   */
+  private static void test1() {
+    // TODO 自動生成されたメソッド・スタブ
+    Vector<Byte[]> v = new Vector<Byte[]>();
+    for (int i = 0;i < 10;i++) {
+      Byte[] b = new Byte[1024 * 1024];
+      v.add(b);
+      System.out.println((i + 1) + "M メモリ利用された");
+    }
+  }
 
 }
--- 01_stackoverflowerror/src/com/stackoverflowerror/MyClass.java
+++ 01_stackoverflowerror/src/com/stackoverflowerror/MyClass.java
@@ -2,34 +2,35 @@ package com.stackoverflowerror;
 
 public class MyClass {
 
-	public  int callmeCount = 0;
+  public  int callmeCount = 0;
 
-	private void callme () {
-		callmeCount++;
-		callme();
-	}
-
-
-	public static void main(String args[]){
+  private void callme() {
+    callmeCount++;
+    callme();
+  }
 
 
-
-		System.out.println("start ");
-
-		MyClass testStack = new MyClass();
-
-
-		try {
-			testStack.callme();
-		} catch (StackOverflowError e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
-
-		System.out.println("callmeCount ="+testStack.callmeCount);
-		System.out.println("end ");
-
-	}
+  /*
+   * main
+   * param args
+   * return void
+   */
+  public static void main(String[] args) {
 
 
+
+    System.out.println("start ");
+
+    MyClass testStack = new MyClass();
+
+
+    try {
+      testStack.callme();
+    } catch (StackOverflowError e) {
+      e.printStackTrace();
+    }
+
+    System.out.println("callmeCount =" + testStack.callmeCount);
+    System.out.println("end ");
+  }
 }
