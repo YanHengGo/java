dd53aed22a7b6f56167be9fae85f010d67404b04
YanHengGo <dang712@hotmail.com>
2018-11-17 18:19:13 +0900

    java.io のデモ
    乱码

---
+++ 12_filedemo/src/com/yanheng/filedemo/MessyCodeDemo.java
@@ -0,0 +1,24 @@
+package com.yanheng.filedemo;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class MessyCodeDemo {
+
+	public static void main(String[] args) throws Exception{
+		//system properties output
+		System.getProperties().list(System.out);
+		System.out.println("-------");
+
+		File file = new File("d:"+File.separator+"99_temp"+File.separator+"messycode.txt");
+		OutputStream outputStream = new FileOutputStream(file);
+		String string = "こんにちは ! \r\n";
+		outputStream.write(string.getBytes());
+		outputStream.write(string.getBytes("ISO8859-1"));
+		outputStream.write(string.getBytes("GBK"));
+		outputStream.write(string.getBytes("UTF-8"));
+		outputStream.close();
+	}
+
+}
